27b78a408520ff4083968657da67cae0f8bc4a18
Daniel Oliveros <icloudaniel764@gmail.com>
2025-03-29 02:13:03 -0500

    Creación de base de datos

---
+++ src/EverPostWebApi/EverPostWebApi/Commons/Dbcontext/EverPostContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EverPostWebApi.Commons.Dbcontext
+{
+    public class EverPostContext : DbContext
+    {
+        public EverPostContext(DbContextOptions<EverPostContext> options) : base(options)
+        { } 
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Categorie> Categories { get; set; }
+        public DbSet<RelPostCategorie> RelPostCategories { get; set; }
+        
+
+
+
+
+    }
+}
